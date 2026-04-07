5c041e37be1caeb8a4ae574f89b58d174143b54b
Shie-creator <beldomiah@naomedical.com>
2026-04-07 23:45:19 +0800

    Delete src directory

--- src/lib/constants.ts
+++
@@ -1,9 +0,0 @@
-export const TERMINATION_TYPES = ["Voluntary", "Involuntary", "Unknown"] as const;
-
-export type TerminationType = (typeof TERMINATION_TYPES)[number];
-
-export const TERMINATION_TYPE_COLORS: Record<TerminationType, string> = {
-  Voluntary: "#11967f",
-  Involuntary: "#db5c3f",
-  Unknown: "#6f8098",
-};
